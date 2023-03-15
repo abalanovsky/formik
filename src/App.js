b3d86158d011b4b5ff1b3593a5b66d41c9be42a6
abalanovsky <ambalanovsky@icloud.com>
2023-03-15 22:03:17 +0200

    Register form added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Yup from "yup";
+import {Field, Formik} from "formik";
 
+const FormValidation = Yup.object().shape({
+  name: Yup.string()
+      .min(2, 'Too short name')
+      .max(17, 'Too long name')
+      .required('Name is required'),
+  email: Yup.string()
+      .email('Incorrect email')
+      .required('Email is required'),
+    password: Yup.string()
+        .min(8,'Must contain 8 and more symbols')
+        .required('Password is required')
+})
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Register form</h1>
+        <Formik
+            initialValues={{ name: '', email: '', password: '' }}
+            validationSchema={FormValidation}
+            validateOnChange={false}
+            validateOnBlur={true}
+            onSubmit={(values) => {
+                alert(`Name: ${values.name}\n Email: ${values.email}\n Password: ${values.password}`);
+            }}
         >
-          Learn React
-        </a>
-      </header>
+          {({
+              errors,
+              touched,
+              handleSubmit,
+              isSubmitting
+            }) => (
+              <form onSubmit={handleSubmit}>
+                  <Field name="name" placeholder="First Name"/>
+                  {errors.name && touched.name ? ( <div className="error-container"> {errors.name} </div>) : null}
+                  <Field type="email" name="email" placeholder="Email" />
+                  {errors.email && touched.email ? ( <div className="error-container"> {errors.email} </div>) : null}
+                  <Field name="password" type="password" placeholder="Password"/>
+                  {errors.password && touched.password ? ( <div className="error-container"> {errors.password} </div>)
+                      : null}
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </form>
+          )}
+        </Formik>
     </div>
   );
 }
